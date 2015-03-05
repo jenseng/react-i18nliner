0ab48adb0ebb0c8d562d84969d7e4aecf96d7c78
Jon Jensen <jenseng@gmail.com>
2015-03-04 23:19:37 -0700

    plan out some more specs

--- __tests__/preprocess.test.js
+++ __tests__/preprocess.test.js
@@ -6,8 +6,21 @@ describe('preprocess', function() {
     expect(subject('<div />')).toEqual('<div />');
   });
 
+  // TODO: don't do a ComponentInterpolator unless there are actually components
   it('transforms Translate into ComponentInterpolator', function() {
     expect(subject('<Translate>hello</Translate>')).toEqual('<ComponentInterpolator string={I18n.t("hello")} />');
   });
+
+  it('concatenates child literals');
+
+  it('creates placeholders for expressions');
+
+  it('creates placeholders for components with no textContent')
+
+  it('ensures placeholders are unique');
+
+  it('creates wrappers for components with textContent');
+
+  it('reuses wrappers for identical components');
 });
 
