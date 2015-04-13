45ded3f19840ff8003e296cf77b20b042dc6a4af
Jon Jensen <jenseng@gmail.com>
2015-04-12 21:31:13 -0600

    ensure webpack/browserify use .i18nrc overrides

--- browserify-transform.js
+++ browserify-transform.js
@@ -1,9 +1,10 @@
 var through = require("through2");
+var i18nliner = require("i18nliner");
 var preprocess = require("./preprocess");
 
 module.exports = function(file) {
   return through(function (buf, enc, next) {
-    this.push(preprocess(buf.toString('utf8')));
+    this.push(preprocess(buf.toString('utf8'), i18nliner));
     next();
   });
 };
--- webpack-loader.js
+++ webpack-loader.js
@@ -1,6 +1,7 @@
+var i18nliner = require("i18nliner");
 var preprocess = require("./preprocess");
 
 module.exports = function(source) {
   this.cacheable();
-  return preprocess(source);
+  return preprocess(source, i18nliner);
 };
