75db61e994ba93d544d95d998f421b28082cbcae
Jon Jensen <jenseng@gmail.com>
2015-04-12 22:14:42 -0600

    ensure i18nrc prefs are plumbed down at extraction time

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ var fs = require("fs");
 var preprocess = require("./preprocess");
 
 module.exports = function(i18nliner) {
-  var JsProcessor = i18nliner.processors.JsProcessor;
-  JsProcessor.prototype.sourceFor = function(file) {
-    return preprocess(fs.readFileSync(file).toString());
-  };
-
   i18nliner.autoTranslateTags = [];
   i18nliner.neverTranslateTags = [];
+
+  var JsProcessor = i18nliner.processors.JsProcessor;
+  JsProcessor.prototype.sourceFor = function(file) {
+    return preprocess(fs.readFileSync(file).toString(), i18nliner);
+  };
 };
