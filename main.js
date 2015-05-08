93848a4f86c9915e90219750b84e5c3a0fb5fdf4
Jon Jensen <jenseng@gmail.com>
2015-05-08 00:49:50 -0600

    tie into i18nliner skip logic
    don't parse the file if we don't need to

--- main.js
+++ main.js
@@ -2,11 +2,37 @@ var fs = require("fs");
 var preprocess = require("./preprocess");
 var recast = require("recast");
 
+var escapeRegExp = function(str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
+
+var getTranslatePattern = function(config) {
+  var pattern = 'translate=["\']yes["\']';
+  var parts = config.autoTranslateTags || [];
+  if (parts.length)
+    pattern += "|<" + parts.map(escapeRegExp).join("|<");
+  return new RegExp(pattern);
+};
+
 module.exports = function(i18nliner) {
   var JsProcessor = i18nliner.processors.JsProcessor;
   var config = i18nliner.config;
+  var origPreProcess = JsProcessor.prototype.preProcess;
+  var pattern;
+
   JsProcessor.prototype.preProcess = function(source) {
-    var ast = recast.parse(source, config.recastOptions);
-    return {ast: preprocess.ast(ast, config)};
+    var fileData = origPreProcess.call(this, source);
+    pattern = pattern || getTranslatePattern(config);
+
+    // avoid a parse if we can
+    fileData.skip = fileData.skip && !source.match(pattern);
+
+    if (!fileData.skip) {
+      var ast = fileData.ast || recast.parse(source, config.recastOptions);
+      preprocess.ast(ast, config);
+      fileData.ast = ast;
+    }
+
+    return fileData;
   };
 };
