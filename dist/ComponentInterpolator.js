1c88448217e1264329b67ceeb0d119a445b4c7c6
Ahmad Amireh <ahmad@amireh.net>
2016-05-26 11:26:57 +0300

    Be compatible with React 0.14, use "invariant" package

--- dist/ComponentInterpolator.js
+++ dist/ComponentInterpolator.js
@@ -1,5 +1,5 @@
 var React = require('react');
-var invariant = require('react/lib/invariant');
+var invariant = require('invariant');
 var $__0=     React.PropTypes,string=$__0.string,object=$__0.object;
 
 var WRAPPER_PATTERN = /(\*+)/;
